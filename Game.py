fc23714ab22a2b9d45956f70ecc794b577dbad69
tthomp10 <tthomp10@terpmail.umd.edu>
2026-04-24 11:24:35 -0400

    Add files via upload

---
+++ Game.py
@@ -0,0 +1,18 @@
+class GameConditions:
+    def __init__(self, dice):
+        self.dice = dice
+    
+    def calculate_score(self):
+        pass
+    
+    def count_matches(self):
+        pass
+    
+    def check_even_odd(self):
+        pass
+    
+    def check_blister(self):
+        pass
+    
+    def check_all_sixes(self):
+        pass
