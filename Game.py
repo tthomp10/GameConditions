27ec2e214b8f3dfa6f7f6cd157378b1e4728848b
tthomp10 <tthomp10@terpmail.umd.edu>
2026-05-12 15:39:56 -0400

    Add files via upload

--- Game.py
+++ Game.py
@@ -3,16 +3,50 @@ class GameConditions:
         self.dice = dice
     
     def calculate_score(self):
-        pass
-    
+        score = 0
+        for i in range(len(self.dice)):
+            for j in range(i + 1, len(self.dice)):
+                color1, val1 = self.dice[i]
+                color2, val2 = self.dice[j]
+                if val1 == val2:
+                    if color1 == color2:
+                        score += val1 * 10
+                    else:
+                        score += val1 + val2
+        return score
+
     def count_matches(self):
-        pass
-    
+        matches = 0
+        for i in range(len(self.dice)):
+            for j in range(i + 1, len(self.dice)):
+                color1, val1 = self.dice[i]
+                color2, val2 = self.dice[j]
+                if val1 == val2:
+                    matches += 1
+        return matches
+
     def check_even_odd(self):
-        pass
+        even = 0
+        odd = 0
+        for color, val in self.dice:
+            if val % 2 == 0:
+                even += 1
+            else:
+                odd += 1
+        return even, odd
+        
     
     def check_blister(self):
-        pass
+        values = []
+        for color, val in self.dice:
+            values.append(val)
+        values.sort()
+        if values == [1, 2, 3, 4, 5] or values == [2, 3, 4, 5, 6]:
+            return True
+        return False
     
     def check_all_sixes(self):
-        pass
+        for color, val in self.dice:
+            if val != 6:
+                return False
+        return True
